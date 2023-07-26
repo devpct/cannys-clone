978a14d757d463e8a508c7ef88136beb06afc263
Mohammad Abdollahzadeh <mr.mohammad.pg19@gmail.com>
2023-07-26 20:06:39 +0330

    debug

--- Frontend/src/router.jsx
+++ Frontend/src/router.jsx
@@ -1,12 +1,12 @@
 import SignupLogin from './pages/signupLogin/SignupLogin'
 import Home from './pages/home/Home.jsx'
-import NotFound from './pages/notfound/NotFound.jsx'
+// import NotFound from './pages/notfound/NotFound.jsx'
 
 let router = [
     {path: '/', element:<Home/>},
     {path: 'login' , element:<SignupLogin/>},
     {path: 'signup' , element:<SignupLogin/>},
-    {path: '*' , element:<NotFound/>}
+    // {path: '*' , element:<NotFound/>}
 ]
 
 export default router
