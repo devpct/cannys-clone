d07c6e074f7be4e705eea2aac42835b9c32a9cad
Mohammad Abdollahzadeh <mrdevpct@gmail.com>
2023-07-26 15:18:16 +0330

    debug

--- Backend/services/emailService.js
+++ Backend/services/emailService.js
@@ -1,7 +1,7 @@
-const nodemailer = require('nodemailer');
-const crypto = require('crypto');
+const nodemailer = require('nodemailer')
+const crypto = require('crypto')
 
-const { host, port, user, pass } = require('../config/smtp');
+const { host, port, user, pass } = require('../config/smtp')
 
 // Create a nodemailer transporter
 const transporter = nodemailer.createTransport({
@@ -12,7 +12,7 @@ const transporter = nodemailer.createTransport({
       user: user,
       pass: pass,
     },
-});
+})
 
 // Function to send the email
 const sendEmail = (to, subject, htmlContent) => {
@@ -24,12 +24,12 @@ const sendEmail = (to, subject, htmlContent) => {
             html: htmlContent,
         }, (error, info) => {
             if (error) {
-                reject(error);
+                reject(error)
             } else {
-                resolve(info);
+                resolve(info)
             }
-        });
-    });
+        })
+    })
 }
 
 const generateVerificationCode = () => {
@@ -38,28 +38,28 @@ const generateVerificationCode = () => {
 }
 
 const sendVerifyCode = (req, res) => {
-    const { email } = req.body;
-    const subject = 'Verification Code';
+    const { email } = req.body
+    const subject = 'Verification Code'
     const code = generateVerificationCode()
 
     const htmlContent = `
     <h1>Hello!</h1>
     <h2>Your verification code is: ${code}</h2>
     <img src="https://articleabode.com/wp-content/uploads/2022/08/3-Best-Canny.io-alternatives-to-Try-right-now-1.png" style="width: 19rem" />
-    `;
+    `
 
     try {
-        sendEmail(email, subject, htmlContent);
+        sendEmail(email, subject, htmlContent)
         res.json({ message: 'Verification code sent successfully', verificationCode: code })
     } catch (error) {
-        handleErrorResponse(res, error);
+        handleErrorResponse(res, error)
     }
 }
 
 
 // Function to handle error responses
 const handleErrorResponse = (res, error) => {
-    res.status(500).json({ message: error.message });
+    res.status(500).json({ message: error.message })
 }
 
 module.exports = {
